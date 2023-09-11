370fa6239ce5a1d0b9274f3fb694e95d66833187
Nahom Zewdu <nahiyo86@gmail.com>
2023-09-11 19:15:58 +0800

    Added grade choice field for student model

--- SchoolAdmin/base/models.py
+++ SchoolAdmin/base/models.py
@@ -5,8 +5,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Student(models.Model):
+    GRADE_CHOICES = [
+        ('5', 'Grade 5'),
+        ('6', 'Grade 6'),
+        ('7', 'Grade 7'),
+        ('8', 'Grade 8'),
+        ('9', 'Grade 9'),
+        ('10', 'Grade 10'),]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE) # One to one relationship with user model
     student_id = models.CharField(max_length=6, unique=True, editable=False)
     age = models.CharField(max_length=2)
-
-    
+    grade = models.CharField(max_length=2, choices=GRADE_CHOICES)
