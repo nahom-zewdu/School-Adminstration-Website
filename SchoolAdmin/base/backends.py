8cd55225c4000332390f9e26c2dc319e94278ada
Nahom Zewdu <nahiyo86@gmail.com>
2023-09-11 19:58:40 +0800

    Created backend Authentication for students

---
+++ SchoolAdmin/base/backends.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model     #returns the User model that is active in these project
+from .models import Student
+
+
+class StudentAuthBackend(ModelBackend):
+    def authenticate(self, request, **kwargs):
+        UserModel = get_user_model()
+        student_id = kwargs.get('student_id')
+
+        try:
+            studet = Student.objects.get(student_id=student_id)
+        except Student.DoesNotExist:
+            return None
+        else:
+            if student.user.check_password(kwargs.get('password')):
+                return student.user
+        
+        return None
+
+
+
