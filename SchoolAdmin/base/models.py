bd182c28be1af8223e4ffed393c83ee76c884a8f
Nahom Zewdu <nahiyo86@gmail.com>
2023-09-11 19:14:01 +0800

    Created a model class for Students

--- SchoolAdmin/base/models.py
+++ SchoolAdmin/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # One to one relationship with user model
+    student_id = models.CharField(max_length=6, unique=True, editable=False)
+    age = models.CharField(max_length=2)
+
+    
