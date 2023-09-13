5a7fd314ab40446795bb4b35da5e3e45ffe6d8eb
Nahom Zewdu <nahiyo86@gmail.com>
2023-09-13 19:42:57 +0800

    registered Parent model for admin site

--- SchoolAdmin/base/admin.py
+++ SchoolAdmin/base/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Student, Teacher
+from .models import Student, Teacher, Parent
 
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Teacher)
+admin.site.register(Parent)
