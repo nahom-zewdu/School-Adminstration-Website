aaf7ef89e30862a54142b568a81beda6ff4fab7d
Nahom Zewdu <nahiyo86@gmail.com>
2023-09-16 07:01:07 +0800

    Created a login view for students

--- SchoolAdmin/base/views.py
+++ SchoolAdmin/base/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {'MTA': 'MTA'})
+
+
+def student_login(request):
+    if request.method == 'POST':
+        student_id = request.POST['student_id']
+        password = request.POST['password']
+
+        user = authenticate(request, student_id=student_id, password=password)
+        if user:
+            login(request, user)
+            return redirect('base:home')
+        else:
+            error_message = 'Invalid ID number or Password'
+            return render(request, 'student_login.html', {'error_message': error_message})
+    else:
+        return render(request, 'student_login.html')
