fca693c3b8672504e3976e1e27eb628a8b3a8cdb
Nahom Zewdu <nahiyo86@gmail.com>
2023-09-20 20:03:18 +0800

    added url route for restricted view

--- SchoolAdmin/base/urls.py
+++ SchoolAdmin/base/urls.py
@@ -6,8 +6,10 @@ app_name = 'base'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('academics/', views.academics, name='academics'),
-
+    path('student_academics/', views.student_academics, name='student_academics'),
+    path('teacher_academics/', views.teacher_academics, name='teacher_academics'),
+    
+    path('restricted/', views.restricted_view, name='restricted'),
     path('student_login/', views.student_login, name='student_login'),
     path('teacher_login/', views.teacher_login, name='teacher_login'),
     path('parent_login/', views.parent_login, name='parent_login'),
