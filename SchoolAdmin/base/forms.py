11b5757d0a080b02a7c4131a6e84d3b2ed60462b
Nahom Zewdu <nahiyo86@gmail.com>
2023-09-22 02:35:04 +0800

    Modified forms.py for the updated Grade model

--- SchoolAdmin/base/forms.py
+++ SchoolAdmin/base/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Grade_7_8
+from .models import Grade
 
-class Grade_7_8_Form(forms.ModelForm):
+class GradeForm(forms.ModelForm):
     class Meta:
-        model = Grade_7_8
-        fields = ['physics', 'chemistry', 'biology']
+        model = Grade
+        fields = '__all__'
