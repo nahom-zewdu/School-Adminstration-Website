b64f0dd5a97c9abd95ea7fbfa6cb71b78768ac7a
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-23 22:48:51 +0800

    Created a custome form for registering a student

--- SchoolAdmin/base/forms.py
+++ SchoolAdmin/base/forms.py
@@ -1,5 +1,25 @@
 from django import forms
-from .models import Subject
+from django.contrib.auth.models import User
+from .models import Subject, Student
+
+
+class StudentCreationForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = visible.field.label
+
+    first_name = forms.CharField(label='First Name', max_length=20, required=True)
+    last_name = forms.CharField(label='Last Name', max_length=20, required=True)
+    email = forms.EmailField(label='Email', required=False)
+
+    class Meta:
+        model = Student
+        fields = ['first_name', 'last_name','email', 'age', 'gender', 'grade', 'parent_phone']
+
+
 
 class SubjectForm(forms.ModelForm):
     class Meta:
