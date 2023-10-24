12fa64aff1619f9d2029643a065a6ccc0c0bc285
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-24 17:15:02 +0800

    Handled the url routing for parent dashboard

--- SchoolAdmin/base/urls.py
+++ SchoolAdmin/base/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('student_dashboard/', views.student_dashboard, name='student_dashboard'),
     path('teacher_dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
     path('staff_dashboard/', views.staff_dashboard, name='staff_dashboard'),
+    path('parent_dashboard/', views.parent_dashboard, name='parent_dashboard'),
 ]
 
 
