e2889b1dc8333b21debcb7eb98ad206c3dbbdf68
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-29 14:49:23 +0800

    Updated admin site

--- SchoolAdmin/base/admin.py
+++ SchoolAdmin/base/admin.py
@@ -1,18 +1,10 @@
 from django.contrib import admin
-from django.forms.models import inlineformset_factory
 from .models import Student, Teacher, Parent, Score, Staff
 from .forms import ScoreForm
 
 
-class ScoreInline(admin.StackedInline):
-    model = Score
-    form = ScoreForm
-
-class StudentAdmin(admin.ModelAdmin):
-    inlines = [ScoreInline]
-
 # Register your models here.
-admin.site.register(Student, StudentAdmin)
+admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Parent)
 admin.site.register(Staff)
