7a7afd09e6bed70eabcf51f967a425988c9cc702
Nahom Zewdu <nahiyo86@gmail.com>
2023-11-05 04:09:51 +0800

    Creanted a list view for announcements

--- SchoolAdmin/announcement/views.py
+++ SchoolAdmin/announcement/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import *
 # Create your views here.
+
+class AnnouncementList(ListView):
+    queryset = Announcement.objects.order_by('-timestamp')
+    context_object_name = 'announcements'
+    template_name = 'announcement/announcement.html'
