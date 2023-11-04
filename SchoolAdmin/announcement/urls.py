560c4cd61969d070434b7c0f58fcd18a2280a09a
Nahom Zewdu <nahiyo86@gmail.com>
2023-11-05 04:11:26 +0800

    Created a template to render announcements

--- SchoolAdmin/announcement/urls.py
+++ SchoolAdmin/announcement/urls.py
@@ -3,10 +3,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from . import views
 app_name = 'announcement'
 
 urlpatterns = [
-
+    path('', views.AnnouncementList.as_view(), name='announcement')
 ]
 
 
