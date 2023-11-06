9a21b59b961f6cc523e298fe11ec9052d43d2a82
Nahom Zewdu <nahiyo86@gmail.com>
2023-11-06 22:22:41 +0800

    created a view for deleting announcements

--- SchoolAdmin/announcement/views.py
+++ SchoolAdmin/announcement/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView
 from .models import *
 # Create your views here.
 
@@ -7,3 +8,11 @@ class AnnouncementList(ListView):
     queryset = Announcement.objects.order_by('-timestamp')
     context_object_name = 'announcements'
     template_name = 'announcement/announcement.html'
+
+
+class DeleteAnnouncement(DeleteView):
+    model = Announcement
+    context_object_name = 'announcement'
+    template_name = 'announcement/delete_announcement.html'
+    success_url = reverse_lazy('announcement:announcement')
+
